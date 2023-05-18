22a681593be6ae8ba0268860e2393f0bb24c740d
c4lyp5o <calypso.hellhammer@gmail.com>
2023-05-18 14:23:08 +0800

    keluar kan error kt console

--- pages/api/save.js
+++ pages/api/save.js
@@ -30,10 +30,11 @@ saveAPI.post(async ({ body: { id, pastedText, dad, mode } }, res) => {
           dad: Uploads.dad,
         });
       } catch (error) {
+        console.log(error);
         res.status(500).json({
           status: 'Failed',
           code: 500,
-          error: 'Internal server error!',
+          msg: error,
         });
       }
       break;
--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,9 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
       <main className='flex flex-col items-center justify-center w-full h-screen bg-black'>
-        <div className='m-4'>
-          <h1 className='text-6xl font-bold text-white'>EZJOT</h1>
-          <p className='text-2xl text-white'>jot down anything</p>
+        <div className='m-4  text-white'>
+          <h1 className='text-6xl font-bold'>EZJOT</h1>
+          <p className='text-2xl'>jot down anything</p>
         </div>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-3 items-center justify-center w-full md:w-3/4 h-full'>
           <Textbox />
