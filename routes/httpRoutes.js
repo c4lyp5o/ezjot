06ea6d3bc5eedeb330fe2d0ce647e8f0401ee6d8
c4lyp5o <calypso@calypsocloud.one>
2025-05-15 00:31:13 +0800

    fix: removed simpleAuth, change purge route to delete method

--- routes/httpRoutes.js
+++ routes/httpRoutes.js
@@ -6,7 +6,6 @@ import {
   getText,
   purgeEverything
 } from '../controllers/httpControllers.js';
-import simpleAuth from '@/middlewares/simpleAuth.js';
 import localAuth from '@/middlewares/localAuth.js';
 
 const router = Router();
@@ -14,6 +13,6 @@ const router = Router();
 router.get('/healthcheck', localAuth, healthCheck);
 router.post('/save', limiter, saveText);
 router.post('/get', limiter, getText);
-router.post('/purge', localAuth, purgeEverything);
+router.delete('/purge', localAuth, purgeEverything);
 
 export default router;
