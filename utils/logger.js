dfeed4951db278e9384fceecc9bba532f78118b9
c4lyp5o <calypso@calypsocloud.one>
2025-05-14 23:15:00 +0800

    fix: add logger

---
+++ utils/logger.js
@@ -0,0 +1,10 @@
+import deadslog from 'deadslog';
+
+const logger = deadslog({
+    fileOutput: {
+        enabled: true,
+        logFilePath: './logs/ezjot.log',
+    }
+});
+
+export default logger;
