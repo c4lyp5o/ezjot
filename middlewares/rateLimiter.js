fa90f380ae37948d8a1fd79f12627f7652526cc1
c4lyp5o <calypso@calypsocloud.one>
2025-05-15 00:31:33 +0800

    fix: a word

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit';
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 50, // Limit each IP to 25 requests per windowMs
+  max: 50, // Limit each IP to 50 requests per windowMs
   message: 'Too many requests from this IP, please try again after a minute',
 });
 
