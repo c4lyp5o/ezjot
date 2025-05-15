463bf72ba095640dd84d1c62f05f8cd3c6cf1b5c
c4lyp5o <calypso@calypsocloud.one>
2025-05-15 09:08:09 +0800

    fix: add head for pages

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -3,19 +3,27 @@ import GetText from "../components/GetText";
 
 function Landing() {
 	return (
-		<div className="flex flex-col h-screen">
-			<header className="bg-gray-800 text-white text-center py-4 shadow-md">
-				<h1 className="text-xl font-bold">EZJOT 2.0 - jot down anything</h1>
-			</header>
-			<div className="grid grid-cols-1 sm:grid-cols-2 flex-1">
-				<div className="border-t border-gray-300 overflow-hidden bg-gray-900 p-4 sm:p-8 sm:border-t-0 sm:border-l">
-					<SaveText />
-				</div>
-				<div className="border-t border-gray-300 overflow-hidden bg-gray-900 p-4 sm:p-8 sm:border-t-0 sm:border-l">
-					<GetText />
+		<>
+			<title>EZJOT</title>
+			<meta name="description" content="Jot down anything" />
+			<meta name="keywords" content="jot, down, ezjot, pastebin" />
+			<meta name="author" content="c4lyp5o" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<link rel="icon" href="/favicon.ico" />
+			<div className="flex flex-col h-screen">
+				<header className="bg-gray-800 text-white text-center py-4 shadow-md">
+					<h1 className="text-xl font-bold">EZJOT 2.0 - jot down anything</h1>
+				</header>
+				<div className="grid grid-cols-1 sm:grid-cols-2 flex-1">
+					<div className="border-t border-gray-300 overflow-hidden bg-gray-900 p-4 sm:p-8 sm:border-t-0 sm:border-l">
+						<SaveText />
+					</div>
+					<div className="border-t border-gray-300 overflow-hidden bg-gray-900 p-4 sm:p-8 sm:border-t-0 sm:border-l">
+						<GetText />
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,17 +1,25 @@
 function NotFound() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-4">
-			<h1 className="text-4xl font-bold text-gray-800 mb-4">404</h1>
-			<p className="text-lg text-gray-600 mb-6">
-				Oops! The page you're looking for doesn't exist.
-			</p>
-			<a
-				href="/"
-				className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
-			>
-				Go Back Home
-			</a>
-		</div>
+		<>
+			<title>Not Found</title>
+			<meta name="description" content="Jot down anything" />
+			<meta name="keywords" content="jot, down, ezjot, pastebin" />
+			<meta name="author" content="c4lyp5o" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<link rel="icon" href="/favicon.ico" />
+			<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-4">
+				<h1 className="text-4xl font-bold text-gray-800 mb-4">404</h1>
+				<p className="text-lg text-gray-600 mb-6">
+					Oops! The page you're looking for doesn't exist.
+				</p>
+				<a
+					href="/"
+					className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+				>
+					Go Back Home
+				</a>
+			</div>
+		</>
 	);
 }
 
