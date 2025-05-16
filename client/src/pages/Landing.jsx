b8da1d2e05f19293450dc8dd73696d046293f646
c4lyp5o <calypso@calypsocloud.one>
2025-05-17 02:58:36 +0800

    fix: add exclamation

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -12,7 +12,7 @@ const Landing = () => {
 			<link rel="icon" href="/favicon.ico" />
 			<div className="flex flex-col h-screen">
 				<header className="bg-gray-800 text-white text-center py-4 shadow-md">
-					<h1 className="text-xl font-bold">EZJOT 2.0 - jot down anything</h1>
+					<h1 className="text-xl font-bold">EZJOT 2.0 - jot down anything!</h1>
 				</header>
 				<div className="grid grid-cols-1 sm:grid-cols-2 flex-1">
 					<div className="border-t border-gray-300 overflow-hidden bg-gray-900 p-4 sm:p-8 sm:border-t-0 sm:border-l">
